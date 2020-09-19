1cb9103c46edf14269fdba127ddc6de204234fa3
rafacanoaudio <rafacanoaudio@gmail.com>
2020-09-19 11:22:19 -0500

    on part 4

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -5,6 +5,11 @@ type: string;
 operational: boolean;
 launchDate: string;    
 
-constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean)
+constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+this.name = name;
+this.type = type;
+this.launchDate = launchDate;
+this.orbitType = orbitType;
+this.operational = operational;
 }
-//comment second test
+};
