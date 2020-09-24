ade0184c32ddda4921fea7c642068800eacb0ea0
rafacanoaudio <rafacanoaudio@gmail.com>
2020-09-24 15:27:32 -0500

    finished

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -3,7 +3,7 @@ name: string;
 orbitType: string;
 type: string;
 operational: boolean;
-launchDate: string;    
+launchDate: string;
 
 constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
 this.name = name;
@@ -13,9 +13,8 @@ this.orbitType = orbitType;
 this.operational = operational;
 }
 
-// shouldShowWarning(): boolean;
-// if (this.type.toLowerCase === 'space debris') {
-//     shouldShowWarning = true;
-// }
+shouldShowWarning(): boolean  {
+return this.type.toLowerCase() === "space debris";
+};
 
 };
